82b97909c4aa6f96d19700d8ad92a1666cc60abc
Sergio Ravera <sergioravera@hotmail.de>
2025-08-13 14:52:29 +0200

    added route and view for sign in page

--- todos.js
+++ todos.js
@@ -301,6 +301,12 @@ app.post("/lists/:todoListId/edit",
   })
 );
 
+// Render the Sign In page.
+app.get("/users/signin", (req, res) => {
+  req.flash("info", "Please sign in.");
+  res.render("signin", { flash: req.flash() });
+});
+
 // Error handler
 app.use((err, req, res, _next) => {
   console.log(err); // Writes more extensive information to the console log
